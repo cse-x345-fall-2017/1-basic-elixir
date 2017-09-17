74db4a4b1393462c12fe30f72006c4cd07f783b7
shashi tekula <stekula@smu.edu>
2017-09-16 20:08:21 -0500

    Ex03 example noughts and cross game working

--- ex03.ex
+++ ex03.ex
@@ -160,7 +160,15 @@ defmodule Ex03 do
   Think a little about a nice way to lay this code out.
   """
 
-   def won . . . "your code"
+  def won({ a, a, a, _, _, _, _, _, _ }), do: a
+  def won({ _, _, _, a, a, a, _, _, _ }), do: a
+  def won({ _, _, _, _, _, _, a, a, a }), do: a
+  def won({ a, _, _, a, _, _, a, _, _ }), do: a
+  def won({ _, a, _, _, a, _, a, _, _ }), do: a
+  def won({ _, _, a, _, _, a, _, _, a }), do: a
+  def won({ a, _, _, _, a, _, _, _, a }), do: a
+  def won({ _, _, a, _, a, _, a, _, _ }), do: a
+  def won( _ ), do: false
 
 
   ###########################
